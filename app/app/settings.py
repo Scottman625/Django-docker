c1e0a3d21978924b88a64d02ab0b78e88edb6c2d
ScottFang <scottman608@gmail.com>
2022-11-21 11:02:52 +0800

    change kubernetes service

--- app/app/settings.py
+++ app/app/settings.py
@@ -149,26 +149,33 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': os.environ.get('POSTGRES_SERVICE'),
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'PORT': os.environ.get('POSTGRES_PORT'),
-        'USER': os.environ.get('POSTGRES_USER')
-    }
-}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'PORT': 5432,
-#         'HOST': 'db',
+#         'HOST': os.environ.get('POSTGRES_SERVICE_HOST'),
+#         'NAME': os.environ.get('POSTGRES_DB'),
+#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+#         'PORT': os.environ.get('POSTGRES_PORT'),
+#         'USER': os.environ.get('POSTGRES_USER')
+#     }
+# }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'db',
+#         'PASSWORD': 'admin',
+#         'PORT': '8000',
+#         'HOST': 'localhost',
 #         'USER': 'postgres',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
